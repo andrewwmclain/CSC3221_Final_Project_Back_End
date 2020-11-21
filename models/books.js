52abddfb5bd95e497b08c9badad6401c2ba66d90
andrewwmclain <mclaina1@spu.edu>
2020-11-21 12:28:15 -0800

    added book schema

---
+++ models/books.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let BookSchema = new Schema({
+    Name: String,
+    Author: String,
+    ISBN: String,
+    Price: Double
+});
+
+module.exports = mongoose.model('Book', BookSchema);
