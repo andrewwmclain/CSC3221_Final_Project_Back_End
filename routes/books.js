20ef74afbbd5f16033d8f04d93162262f93a20fd
andrewwmclain <mclaina1@spu.edu>
2020-11-27 21:03:44 -0800

    ISBN validation for each action

--- routes/books.js
+++ routes/books.js
@@ -10,46 +10,21 @@ function HandleError(response, reason, message, code){
     response.status(code || 500).json({"error:": message});
 }
 
-    // Name: String,
-    // Author: String,
-    // ISBN: String,
-    // Price: Double
-
-
 router.post('/', (request, response, next) => {
     response.header("Access-Control-Allow-Origin", "*");
     response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
     let obj = JSON.parse(JSON.stringify(request.body));
-
-    // let newBook = request.body;
-    // console.log(request.body);
-
     let newBook = obj;
-    console.log(newBook);
+    // console.log(newBook);
 
-    // console.log("hi!");
     if (!newBook.Name || !newBook.Author || !newBook.ISBN || !newBook.Price){
         HandleError(response, 'Missing Info', 'Form data missing', 500);
     }else{
-        // var re = /^(?:ISBN(?:-1[03])?:? )?(?=[0-9X]{10}$|(?=(?:[0-9]+[- ]){3})[- 0-9X]{13}$|97[89][0-9]{10}$|(?=(?:[0-9]+[- ]){4})[- 0-9]{17}$)(?:97[89][- ]?)?[0-9]{1,5}[- ]?[0-9]+[- ]?[0-9]+[- ]?[0-9X]$/;
-
-        // var re = /^(97(8|9))?\d{9}(\d|X)$/;
-        // var re2 = new RegExp('' +
-        //     /^(?:ISBN(?:-1[03])?:? )?(?=[0-9X]{10}$|/ +
-        //     /(?=(?:[0-9]+[- ]){3})/.source +
-        //     /[- 0-9X]{13}$|97[89][0-9]{10}$|/.source +
-        //     /(?=(?:[0-9]+[- ]){4})[- 0-9]{17}$)(?:97[89][- ]?)?[0-9]{1,5}[- ]?[0-9]+[- ]?[0-9]+[- ]?[0-9X]$/.source
-        // );
-
-        // var match = re.exec(newBook.ISBN);
-        // var re = /^(?:ISBN(?:-13)?:?\ )?(?=[0-9]{13}$|(?=(?:[0-9]+[-\ ]){4})[-\ 0-9]{17}$)97[89][-\ ]?[0-9]{1,5}[-\ ]?[0-9]+[-\ ]?[0-9]+[-\ ]?[0-9]$/;
-
         var re = /^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+$/;
-
         var match = re.exec(newBook.ISBN);
 
-        if(match.length == 0){
+        if(!match){
             HandleError(response, 'Invalid ISBN', 'ISBN format is invalid', 500);
         }else{
             let book = new BookSchema({
@@ -116,17 +91,24 @@ router.get('/:isbn', (request, response, next) =>{
     response.header("Access-Control-Allow-Origin", "*");
     response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-    BookSchema
-        .find({"ISBN": request.params.isbn}, (error, result) =>{
-            if (error) {
-                response.status(500).send(error);
-            }
-            if (result && result.length > 0){
-                response.send(result);
-            }else{
-                response.status(404).send({"isbn": request.params.isbn, "error":  "Not Found"});
-            }
-        });
+    var re = /^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+$/;
+    var match = re.exec(request.params.isbn);
+
+    if(!match){
+        HandleError(response, 'Invalid ISBN', 'ISBN format is invalid', 500);
+    }else{
+        BookSchema
+            .find({"ISBN": request.params.isbn}, (error, result) =>{
+                if (error) {
+                    response.status(500).send(error);
+                }
+                if (result && result.length > 0){
+                    response.send(result);
+                }else{
+                    response.status(404).send({"isbn": request.params.isbn, "error":  "Not Found"});
+                }
+            });
+    }
 });
 
 // router.patch('/:id', (request, response, next) =>{
@@ -158,50 +140,64 @@ router.patch('/:isbn', (request, response, next) =>{
     response.header("Access-Control-Allow-Origin", "*");
     response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-    BookSchema
-        .findOne({"ISBN": request.params.isbn}, (error, result)=>{
-            if (error) {
-                response.status(500).send(error);
-                console.log("test error");
-            }else if (result){
-                if (request.body.isbn){
-                    delete request.body.isbn;
-                }
-                for (let field in request.body){
-                    result[field] = request.body[field];
-                }
-                result.save((error, friend)=>{
-                    if (error){
-                        response.status(500).send(error);
-                    }
-                    response.send(friend);
-                });
-            }else{
-                response.status(404).send({"ISBN": request.params.isbn, "error":  "Not Found"});
-            }
+    var re = /^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+$/;
+    var match = re.exec(request.params.isbn);
 
-        });
+    if(!match){
+        HandleError(response, 'Invalid ISBN', 'ISBN format is invalid', 500);
+    }else {
+        BookSchema
+            .findOne({"ISBN": request.params.isbn}, (error, result) => {
+                if (error) {
+                    response.status(500).send(error);
+                    console.log("test error");
+                } else if (result) {
+                    if (request.body.isbn) {
+                        delete request.body.isbn;
+                    }
+                    for (let field in request.body) {
+                        result[field] = request.body[field];
+                    }
+                    result.save((error, friend) => {
+                        if (error) {
+                            response.status(500).send(error);
+                        }
+                        response.send(friend);
+                    });
+                } else {
+                    response.status(404).send({"ISBN": request.params.isbn, "error": "Not Found"});
+                }
+
+            });
+    }
 });
 
 router.delete('/:isbn', (request, response, next) =>{
     response.header("Access-Control-Allow-Origin", "*");
     response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-    BookSchema
-        .findOne({"ISBN": request.params.isbn}, (error, result)=>{
-            if (error) {
-                response.status(500).send(error);
-            }else if (result){
-                result.remove((error)=>{
-                    if (error){
-                        response.status(500).send(error);
-                    }
-                    response.send({"deletedISBN": request.params.isbn});
-                });
-            }else{
-                response.status(404).send({"ISBN": request.params.isbn, "error":  "Not Found"});
-            }
-        });
+    var re = /^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+$/;
+    var match = re.exec(request.params.isbn);
+
+    if(!match){
+        HandleError(response, 'Invalid ISBN', 'ISBN format is invalid', 500);
+    }else {
+        BookSchema
+            .findOne({"ISBN": request.params.isbn}, (error, result) => {
+                if (error) {
+                    response.status(500).send(error);
+                } else if (result) {
+                    result.remove((error) => {
+                        if (error) {
+                            response.status(500).send(error);
+                        }
+                        response.send({"deletedISBN": request.params.isbn});
+                    });
+                } else {
+                    response.status(404).send({"ISBN": request.params.isbn, "error": "Not Found"});
+                }
+            });
+    }
 });
 
 
